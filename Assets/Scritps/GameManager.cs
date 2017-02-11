b828b62d6994267d2640318f149085a62c87edef
sreveloc <sreveloc@gmail.com>
2017-02-11 23:09:59 +0100

    Primera aproximación

---
+++ Assets/Scritps/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public Rotator rotator;
+	public Spawner spawner;
+
+	public Animator animator;
+
+	public int maxScore = 0;
+	public int score = 0;
+
+	public Text scoreLabel;
+	public Text maxScoreLabel;
+
+	public bool gameHasEnded = false;
+
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}
+		else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		int highscore = PlayerPrefs.GetInt("MaxScore");
+		maxScoreLabel.text =  highscore == 0 ? "" : "Max: " + highscore.ToString();
+	}
+
+	public void EndGame() {
+		if (gameHasEnded)
+			return;
+
+		rotator.enabled = false;
+		spawner.enabled = false;
+
+		animator.SetTrigger ("EndGame");
+		if (score > PlayerPrefs.GetInt("MaxScore")) {
+			PlayerPrefs.SetInt("MaxScore", score);
+		}
+
+		gameHasEnded = true;
+	}
+
+	void Update() {
+		scoreLabel.text = score.ToString();
+	}
+
+	public void RestartLevel () {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex  );
+		score = 0;
+		gameHasEnded = false;
+	}
+
+
+}
---
+++ Assets/Scritps/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject pinPrefab;
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1")) {
+			SpawnPin();
+		}
+	}
+
+	void SpawnPin () {
+		GameObject pin = Instantiate(pinPrefab, transform.position, transform.rotation);
+		pin.name = "Pin " + GameManager.instance.score.ToString();
+		Debug.Log ("Instanciado " + pin.name);
+	}
+}
